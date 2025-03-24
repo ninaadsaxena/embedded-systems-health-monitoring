f94fe1ea2a120a234c3565163d3d946c684e9bf8
Ninaad Saxena <144601257+ninaadsaxena@users.noreply.github.com>
2025-03-24 17:58:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.after_request
 def add_security_headers(response):
