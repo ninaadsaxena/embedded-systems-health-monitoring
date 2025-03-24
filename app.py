50e604bc913a356f5386f7d01f9b5b6cab08d8eb
Ninaad Saxena <144601257+ninaadsaxena@users.noreply.github.com>
2025-03-24 17:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def add_security_headers(response):
 
 @app.route('/')
 def index():
-    return render_template('App.js')
+    return render_template('index.html')
 
 @app.route('/metrics')
 def get_metrics():
